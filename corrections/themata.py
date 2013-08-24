329dfd05a00d30485c99fecdafc83dfc2a442ffb
Paul Evans <decretist@gmail.com>
2013-08-24 14:32:00 -0400

    Experiment with csv data

--- corrections/themata.py
+++ corrections/themata.py
@@ -2,6 +2,7 @@
 #
 # Paul Evans (10evans@cardinalmail.cua.edu)
 # 19 March 2013
+# 24 April 2013
 #
 # themata.py | fmt > themata.txt
 #
@@ -9,6 +10,7 @@ import re
 def main():
     f = open('./edF.txt', 'r')
     file = f.read()
+    count = 1
     themata = re.findall('\<2 \d{1,3}\>\<T Q\>(.*?)\<3 \d{1,2}\>', file, re.S)
     # re.S (re.DOTALL) makes '.' special character match any character including newline.
     # print('36: ' + str(len(themata)))
@@ -18,7 +20,9 @@ def main():
         thema = re.sub('\s+', ' ', thema)
         thema = re.sub('^[ ]+', '', thema) # remove leading whitespace characters
         thema = re.sub('[ ]+$', '', thema) # remove trailing whitespace characters
-        print(thema + '\n')
+        # print(thema + '\n')
+        print('{:0=3}'.format(count) + ' _ ' + thema)
+        count = count + 1
 
 if __name__ == '__main__':
     main()
