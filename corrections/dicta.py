c9ae75ecad308a776bfea2c1e0478392fda12cd0
Paul Evans <decretist@gmail.com>
2013-10-17 10:04:27 -0400

    Revised version of dicta.py.

--- corrections/dicta.py
+++ corrections/dicta.py
@@ -1,15 +1,16 @@
 #!/usr/local/bin/python3
 #
 # Paul Evans (10evans@cardinalmail.cua.edu)
-# 11 March 2013
-# 12 March 2013
 #
 # dicta.py | fmt > dicta.txt
 #
 import re
+import sys
 def main():
     f = open('./edF.txt', 'r')
     file = f.read()
+    toc = open('./toc.txt', 'r')
+    dictionary = {}
     # (?<=...) positive lookbehind assertion.
     dicta = re.findall('(?:\<T [AP]\>|(?<=\<T [AP]\>))(.*?)'    # dictum starts with dictum ante or dictum post tag.
         '(?:'                   # non-capturing group.
@@ -20,7 +21,7 @@ def main():
             '\<P 1\>|'          # or Palea,
             '\<T [AIPT]\>'      # or inscriptio or text tag.
         ')', file, re.S)        # re.S (re.DOTALL) makes '.' special character match any character including newline.
-    # print('1279: ' + str(len(dicta)))
+    print('expected 1277 dicta, found ' + str(len(dicta)) + ' dicta', file=sys.stderr)
     for dictum in dicta:
         dictum = re.sub('\<S \d{1,4}\>\<L 1\> \-\d{1,4}\+', '', dictum) # remove page and line number tags.
         dictum = re.sub('\<P 1\> \-\[PALEA\.\+', '', dictum)    # remove Palea tags.
@@ -29,7 +30,28 @@ def main():
         dictum = re.sub('\s+', ' ', dictum)
         dictum = re.sub('^\s+', '', dictum) # remove leading whitespace characters
         dictum = re.sub('\s+$', '', dictum) # remove trailing whitespace characters
-        print(dictum + '\n')
+        key = toc.readline().rstrip()
+        if key in dictionary:
+        # if there's already a dictionary entry with this key, merge the entries
+            print('duplicate key: ' + key, file=sys.stderr)
+            tmp = dictionary[key]
+            dictum = tmp + ' ' + dictum
+        dictionary[key] = dictum
+    count = 1
+    # uniq toc.txt > dedup.txt
+    keys = tuple(open('./dedup.txt', 'r'))
+    for key in keys:
+        key = key.rstrip()
+        print(key + '\n\n' + dictionary[key] + '\n')
+        #
+        # print('{:0=4}'.format(count) + ' _ ' + dictionary[key])
+        # count = count + 1
+        #
+        # outfilename = './tmp/' + key + '.txt'
+        # f = open(outfilename, 'w')
+        # f.write(dictionary[key] + '\n')
+        # f.close
+        #
 
 if __name__ == '__main__':
     main()
