3dfa43a3bfaf83756d22da78a755bf59c8e2124b
Mike Kestemont <mike.kestemont@gmail.com>
2015-04-23 15:59:06 +0200

    HSD

---
+++ stylo/CA_GENRE_HSD.R
@@ -0,0 +1,34 @@
+###### LOAD DATA ###########################################################
+library(ca)
+library(languageR)
+samples = read.table("~/Desktop/code_dissertatie/maerlant/workspace/dimensionReduction.tsv", header=TRUE)
+samples.ca = ca(samples, nd=2)
+#plot(txts.ca, what=c("all", "none"))
+meta = read.table("~/Desktop/code_dissertatie/maerlant/workspace/meta_data.tsv", header=TRUE)
+COOR = data.frame(samples.ca$rowcoord, TEXT=meta$TEXT, GENRE=meta$GENRE)
+############################################################################
+
+
+print("######### FOR FIRST DIMENSION ##########################")
+COOR.aov1 = aov(X1~GENRE, data=COOR)
+print(TukeyHSD(COOR.aov1))
+print(kruskal.test(COOR$X1, COOR$GENRE))
+print("######### FOR SECOND DIMENSION ##########################")
+COOR.aov2 = aov(X2~GENRE, data=COOR)
+print(TukeyHSD(COOR.aov2))
+print(kruskal.test(COOR$X2, COOR$GENRE))
+############################################################################
+###### TukeyHSD FOR POSITION VERSUS GENRE #########################
+par(mfrow=c(1,2))
+plot(TukeyHSD(COOR.aov1), sub=NULL)
+abline(v=0, lty=3)
+plot(TukeyHSD(COOR.aov2))
+abline(v=0, lty=3)
+par(mfrow=c(1,1))
+
+
+
+
+
+
+
