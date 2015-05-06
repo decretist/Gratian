b6ac46c72862653ecba591d5269be449422001ae
Paul Evans <decretist@gmail.com>
2015-05-06 13:30:17 -0400

    First pass at Tukey HSD.

---
+++ stylometry/Tukey.R
@@ -0,0 +1,26 @@
+###### LOAD DATA ###########################################################
+library(ca)
+library(languageR)
+samples = t(read.table("~/Work/Gratian/stylometry/table_with_frequencies.txt", header=TRUE))
+samples.ca = ca(samples)
+#plot(txts.ca, what=c("all", "none"))
+meta = read.table("~/Work/Gratian/stylometry/meta_data.tsv", header=TRUE)
+COOR = data.frame(samples.ca$rowcoord, TEXT=meta$TEXT)
+############################################################################
+
+print("######### FOR FIRST DIMENSION ##########################")
+COOR.aov1 = aov(Dim1~TEXT, data=COOR)
+print(TukeyHSD(COOR.aov1))
+print(kruskal.test(COOR$Dim1, COOR$TEXT))
+print("######### FOR SECOND DIMENSION ##########################")
+COOR.aov2 = aov(Dim2~TEXT, data=COOR)
+print(TukeyHSD(COOR.aov2))
+print(kruskal.test(COOR$Dim2, COOR$TEXT))
+############################################################################
+###### TukeyHSD FOR POSITION VERSUS TEXT #########################
+par(mfrow=c(1,2))
+plot(TukeyHSD(COOR.aov1), sub=NULL)
+abline(v=0, lty=3)
+plot(TukeyHSD(COOR.aov2))
+abline(v=0, lty=3)
+par(mfrow=c(1,1))
