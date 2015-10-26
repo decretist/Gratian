59f17c9cc96f00baea330350190890f972f90aa2
Paul Evans <decretist@gmail.com>
2015-10-25 17:06:30 -0700

    Adding Gratian[12].sha1 to verify file integrity of Gratian[12].txt.

--- samples/rollup.sh
+++ samples/rollup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [ -f Gratian1.txt ];
+then
+  rm -i Gratian1.txt
+fi
+if [ -f Gratian2.txt ];
+then
+  rm -i Gratian2.txt
+fi
 for file in `cat manifest1`
 do
   cat master/g1_$file.txt >> Gratian1.txt
@@ -7,3 +15,6 @@ for file in `cat manifest2`
 do
   cat master/g2_$file.txt >> Gratian2.txt
 done
+shasum -c Gratian1.sha1
+shasum -c Gratian2.sha1
+
