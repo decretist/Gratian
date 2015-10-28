9c641270d12148985469c2c33e3223581b06c820
Paul Evans <decretist@gmail.com>
2015-10-28 09:13:23 -0700

    dicta_2r.py now (mostly) correctly generates the individual dictum debugging files in 2r/.

--- samples/auto/dicta_2r.py
+++ samples/auto/dicta_2r.py
@@ -42,21 +42,11 @@ def main():
     for key in keys:
         key = key.rstrip()
         dictionary_1r[key] = dictionary_Fr[key] # copy dictum from Friedberg dictionary into first-recension dictionary
-        # print(key + '\n' + dictionary_1r[key] + '\n', file=sys.stderr)
-        outfilename_r1 = './1r/' + key + '.txt'
-        f = open(outfilename_r1, 'w')
-        f.write(dictionary_1r[key] + '\n')
-        f.close
 
     keys = tuple(open('../hand/toc_2r.txt', 'r'))
     for key in keys:
         key = key.rstrip()
         dictionary_2r[key] = dictionary_Fr[key] # copy dictum from Friedberg dictionary into second-recension dictionary
-        # print(key + '\n' + dictionary_2r[key] + '\n', file=sys.stderr)
-        outfilename_2r = './2r_pre/' + key + '.txt'
-        f = open(outfilename_2r, 'w')
-        f.write(dictionary_2r[key] + '\n')
-        f.close
 
     keysandpatterns = [
         {'key': 'D.25 d.p.c.1', 'pattern': '(Ex hac epistola liquet, quid cuiusque offitii sit\.)'},
@@ -68,7 +58,7 @@ def main():
         {'key': 'D.45 d.p.c.17', 'pattern': '(Hinc etiam alibi.*?uero patrem exhibeat.")'},
         {'key': 'D.47 d.p.c.8', 'pattern': '(Necesse est etiam.*?sollicitam diligentiam exhibebit\?)'},
         {'key': 'D.54 d.p.c.23', 'pattern': '(Ecce, quomodo serui.*?quomodo non admittantur\.)'},
-        {'key': 'D.63 d.p.c.28', 'pattern': '(Verum, quia inperatores.*?anathematis uinculo innodaretur,).*?(Postremo presentibus legatis.*?ecclesiae Dei conferentes\.)'},
+        {'key': 'D.63 d.p.c.28', 'pattern': '(Verum, quia inperatores.*?anathematis uinculo innodaretur,)(.*?)(Postremo presentibus legatis.*?ecclesiae Dei conferentes\.)'},
         {'key': 'D.63 d.p.c.34', 'pattern': '(Ex his constitutionibus.*?habita constitutum est\.)'},
         {'key': 'D.68 d.p.c.2', 'pattern': '(Quod ergo consecratus.*?ad cautelam salutis\.)'},
         {'key': 'C.1 q.1 d.p.c.51', 'pattern': '(Sed notandum est.*?Non sanat baptismus perfidorum, etc.")'},
@@ -80,7 +70,7 @@ def main():
         {'key': 'C.2 q.6 d.p.c.31', 'pattern': '(Forma uero appellationis.*?in scriptis fieri debent\.)'},
         {'key': 'C.2 q.6 d.p.c.39', 'pattern': '(Cum autem in.*?suam agere oportet\.)'},
         {'key': 'C.2 q.7 d.p.c.40', 'pattern': '(Cum ergo Petrus.*?suscipere reprehensionem subditorum\.)'},
-        {'key': 'C.2 q.8 d.p.c.5', 'pattern': '(Sed Calixtus Papa.*?per epistolam accusare audeat)'},
+        {'key': 'C.2 q.8 d.p.c.5', 'pattern': '(Sed Calixtus Papa.*?per epistolam accusare audeat\.)'},
         {'key': 'C.3 q.1 d.p.c.6', 'pattern': '(Patet ergo, quod.*?quam uocentur ad causam\.)'},
         {'key': 'C.3 q.11 d.p.c.3', 'pattern': '(Hoc autem intelligendum.*?auctoritatibus non prohibetur\.)'},
         # {'key': 'C.4 q.2 d.p.c.3', 'pattern': '(Sed obicitur illud.*?humanae actionis trahenda\.)'},
@@ -88,8 +78,8 @@ def main():
         {'key': 'C.6 q.1 d.p.c.21', 'pattern': '(Verum hoc Augustini.*?accusatione ipse repellit\.)'},
         {'key': 'C.11 q.1 d.p.c.34', 'pattern': '(Non ait propter.*?quam criminalem intelligens\.)'},
         {'key': 'C.11 q.3 d.p.c.40', 'pattern': '(Premissis auctoritatibus, quibus.*?in se exceperunt\.)'},
-        {'key': 'C.13 q.1 d.p.c.1', 'pattern': '(In diocesi autem.*?qui secum erant).*?(Quia ergo nos.*?ad diocesianum transferre\.)'},
-        {'key': 'C.13 q.2 d.p.c.3', 'pattern': '(Item Ioseph, moriens.*?eo sepultus est\?).*?(Exemplo igitur istorum.*?uoluntate tumulandi consistit\.)'},
+        {'key': 'C.13 q.1 d.p.c.1', 'pattern': '(In diocesi autem.*?qui secum erant\.)(.*?)(Quia ergo nos.*?ad diocesianum transferre\.)'},
+        {'key': 'C.13 q.2 d.p.c.3', 'pattern': '(Item Ioseph, moriens.*?eo sepultus est\?)(.*?)(Exemplo igitur istorum.*?uoluntate tumulandi consistit\.)'},
         {'key': 'C.13 q.2 d.p.c.8', 'pattern': '(Hac nimirum auctoritate.*?quam prohibetur transscendere\.)'},
         {'key': 'C.14 q.1 d.p.c.1', 'pattern': '(Quia ergo generaliter.*?prohibentur stare coram iudice\.)'}, # 'stare coram iudice' occurs twice
         {'key': 'C.14 q.2 d.p.c.1', 'pattern': '(Potest etiam intelligi.*?pauperum, testimonium dicant\.)'},
@@ -97,7 +87,7 @@ def main():
         {'key': 'C.15 q.1 d.p.c.3', 'pattern': '(Ex eo autem.*?penam aut gloriam.")'},
         {'key': 'C.15 q.1 d.p.c.11', 'pattern': '(Cum itaque qui.*?Obicitur autem)'},
         {'key': 'C.15 q.1 d.p.c.12', 'pattern': '(Sunt quedam, que.*?muneris executionem inpediunt\.)'},
-        {'key': 'C.16 q.1 d.p.c.40', 'pattern': '(Ostendit ergo Ieronimus.*?ipsum inperfectis connumerans).*?(Ecce sufficienter monstratum.*?assecuntur potestatis executionem\.)'}, # inperfectis
+        {'key': 'C.16 q.1 d.p.c.40', 'pattern': '(.*?)(Ostendit ergo Ieronimus.*?ipsum inperfectis connumerans\.)(.*?)(Ecce sufficienter monstratum.*?assecuntur potestatis executionem\.)(.*?)'}, # inperfectis
         {'key': 'C.16 q.1 d.p.c.47', 'pattern': '(Quod autem dicitur.*?duos potest diuidi,)'},
         {'key': 'C.16 q.1 d.p.c.53', 'pattern': '(Sicut duo episcopatus.*?ad paucitatem redigeretur\.)'},
         {'key': 'C.16 q.3 d.p.c.15', 'pattern': '(Potest etiam aliter.*?obici non potest\.)'},
@@ -111,7 +101,7 @@ def main():
         {'key': 'C.23 q.4 d.p.c.30', 'pattern': '(Quod autem peccatum.*?patienter tollerasse asseritur\.)'},
         {'key': 'C.23 q.8 d.p.c.25', 'pattern': '(Hinc datur intelligi.*?Pontificis fieri debet\.)'},
         {'key': 'C.23 q.8 d.p.c.27', 'pattern': '(Reprehenduntur ergo Gallicani.*?orationibus Deo conmendent\.)'},
-        {'key': 'C.29 q.1 d.a.c.1', 'pattern': '(Quod autem coniugium.*?potest eam dimittere)'},
+        {'key': 'C.29 q.1 d.a.c.1', 'pattern': '(Quod autem coniugium.*?potest eam dimittere,)'},
         {'key': 'C.29 q.2 d.p.c.6', 'pattern': '(Cum dicitur: "sciens.*?fraude decepta est;)'},
         {'key': 'C.30 q.4 d.p.c.5', 'pattern': '(Notandum uero est.*?uiro suo cognoscitur\.)'},
         {'key': 'C.31 q.1 d.p.c.7', 'pattern': '(Sed obicitur: Dauid.*?quam significatione futurorum\.)'},
@@ -119,7 +109,7 @@ def main():
         {'key': 'C.32 q.4 d.p.c.10', 'pattern': '(Ecce, quod nullo.*?nomine iudicantur indigni\.)'},
         {'key': 'C.33 q.2 d.p.c.9', 'pattern': '(In premissis auctoritatibus.*?eis misericordia inpendatur\.)'},
         {'key': 'de Pen. D.1 d.a.c.1', 'pattern': '(Utrum sola cordis.*?promereri, iuxta illud)'}, # d.a.c.1
-        {'key': 'de Pen. D.1 d.p.c.87', 'pattern': '(His auctoritatibus asseritur.*?iugiter confiteri debemus\.).*?(Similiter et illud.*?de penitencia ait:)'},
+        {'key': 'de Pen. D.1 d.p.c.87', 'pattern': '(His auctoritatibus asseritur.*?iugiter confiteri debemus\.)(.*?)(Similiter et illud.*?de penitencia ait:)'},
         {'key': 'de Pen. D.2 d.a.c.1', 'pattern': '(Alii dicunt penitenciam.*?tibi aliquid contingat.")'},
         {'key': 'de Pen. D.2 d.p.c.24', 'pattern': '(Hec itaque karitas.*?redeunt et cetera.")'},
         {'key': 'C.35 q.2 d.p.c.21', 'pattern': '(Hac auctoritate dum.*?ducat in uxorem.")'}, # ducat
@@ -132,8 +122,13 @@ def main():
         if result:
             if len(result.groups()) == 1:
                 dictionary_1r[key] = fixString(result.group(1))
-            elif len(result.groups()) == 2:
-                dictionary_1r[key] = fixString(result.group(1)) + ' ' + fixString(result.group(2))
+                dictionary_2r[key] = fixString(re.sub(pattern, '', dictionary_Fr[key])) # dictionary_Fr or dictionary_2r?
+            elif len(result.groups()) == 3:
+                dictionary_1r[key] = fixString(result.group(1)) + ' ' + fixString(result.group(3))
+                dictionary_2r[key] = fixString(result.group(2))
+            elif len(result.groups()) == 5: # C.16 q.1 d.p.c.40
+                dictionary_1r[key] = fixString(result.group(2)) + ' ' + fixString(result.group(4))
+                dictionary_2r[key] = fixString(result.group(1)) + ' ' + fixString(result.group(3))
             # print(dictionary_1r[key], file=sys.stderr)
         else:
             print('no match: ' + key + '\n' + dictionary_Fr[key], file=sys.stderr)
@@ -166,7 +161,7 @@ def main():
     dictionary_1r['de Pen. D.1 d.a.c.1'] = dictionary_1r[key].rstrip('.') + ''' Leonis pape:'''
 
     all = open('./Gratian1.txt', 'w')
-    keys = tuple(open('./toc_1r.txt', 'r'))
+    keys = tuple(open('./toc_debug.txt', 'r'))
     for key in keys:
         key = key.rstrip()
         outfilename = './1r/' + key + '.txt'
@@ -176,14 +171,28 @@ def main():
         each.close
     all.close()
 
+    all = open('./Gratian2.txt', 'w')
+    keys = tuple(open('./toc_debug.txt', 'r'))
+    for key in keys:
+        key = key.rstrip()
+        outfilename = './2r/' + key + '.txt'
+        each = open(outfilename, 'w')
+        each.write(dictionary_2r[key] + '\n')
+        all.write(dictionary_2r[key] + '\n')
+        each.close
+    all.close()
+
 def fixString(string):
+    string = re.sub('\s+', ' ', string) # 2r
+    string = re.sub('^\s+', '', string) # 2r
+    string = re.sub('\s+$', '', string) # 2r
     if string[0].islower():
-        string = string[0].upper() + string[1:]
+        # string = string[0].upper() + string[1:]
+        pass
     if string[-1] == ',' or string[-1] == ';':
         string = string[0:-1] + '.'
     if string[-1].isalpha():
         string = string + '.'
-    # print(string[0:36] + ' ... ' + string[-36:], file=sys.stderr)
     return string
 
 if __name__ == '__main__':
