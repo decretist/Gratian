4ef1c934fa3f59587148fe9d2cb370da7e7bf0b5
Paul Evans <decretist@gmail.com>
2019-02-03 15:44:35 -0800

    Update Mallet demo
    Change demo script from csh to bash; add README.md documenting how
    to regenerate text files analyzed by Mallet.

---
+++ Mallet/demo/data/README.md
@@ -0,0 +1,10 @@
+## Mallet/demo/data
+
+Regenerate the 850 500-word sample files used in this demo.
+
+```
+mkdir gratian
+cd gratian
+split -a 3 -l 500 ../word_per_line.txt decretum_
+```
+
--- Mallet/demo/demo.sh
+++ Mallet/demo/demo.sh
@@ -1,3 +1,3 @@
-#!/bin/csh
+#!/bin/bash
 mallet import-dir --input data/gratian --output gratian-input.mallet --keep-sequence --stoplist-file stoplists/mgh.txt
 mallet train-topics --optimize-interval 10 --num-topics 20 --num-iterations 1000 --input gratian-input.mallet --output-topic-keys gratian-20-topic-keys.txt --num-top-words 10
