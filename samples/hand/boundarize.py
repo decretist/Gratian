61f41a5baa7cd95f1936010c6baabf6f8a243c64
Paul Evans <decretist@gmail.com>
2020-02-09 12:26:30 -0800

    Modify boundarize.py to handle de Pen. separately.
    boundarize.py can now optionally include or exclude dicta from
    de Pen. in Gratian1.txt and Gratian2.txt.

--- samples/hand/boundarize.py
+++ samples/hand/boundarize.py
@@ -4,20 +4,30 @@
 '''Roll up _dicta_ samples into 2500- to 3000-word chunks'''
 import re
 def main():
-    recension = '1' # either '1' or '2'
+    recension = '2' # either '1' or '2'
+    output_all = False # True equivalent to corpus3, False equivalent to corpus6
+    output_de_Pen = False
+    if output_de_Pen: output_file_basename = f'./tmp/dePen{recension}_'
+    else: output_file_basename = f'./tmp/Gratian{recension}_'
     sequence = 1
     running_total = 0
     dicta = '' # accumulator
     toc_file = open(f'toc_{recension}r.txt', 'r')
     lines = toc_file.readlines()
     for line in lines:
-        output_filename = f'./tmp/Gratian{recension}_' + str(sequence) + '.txt'
         input_filename = line.rstrip()
+        if not output_all:
+            match = re.match(r'^de Pen. D.\d d.[ap].c.\d{1,2}$', input_filename)
+            if output_de_Pen:
+                if not match: continue
+            else:
+                if match: continue
+        output_filename = output_file_basename + str(sequence) + '.txt'
         input_file = open(f'./{recension}r/' + input_filename + '.txt', 'r')
         dictum = input_file.read()
         input_file.close()
         word_count = len(dictum.split())
-        # findall returns same word count as split for all 1r and 2r samples
+        # findall returns same word count as split for all 1r and 2r dicta
         # word_count = len(re.findall(r'\w+', dictum))
         if running_total <= 2500:
             running_total += word_count
@@ -29,7 +39,7 @@ def main():
             sequence += 1
             running_total = word_count
             dicta = dictum
-    # output final incomplete sample
+    # output final incomplete file
     output_file = open(output_filename, 'w')
     output_file.write(dicta)
     output_file.close()
