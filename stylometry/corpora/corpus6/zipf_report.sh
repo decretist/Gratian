46d11693466072800e5a7f83e005e024b9a6b57e
Paul Evans <decretist@gmail.com>
2020-02-13 15:33:05 -0800

    Add script to collect rank-frequency data
    for Zipf's law analysis

---
+++ stylometry/corpora/corpus6/zipf_report.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Paul Evans (10evans@cua.edu)
+# 13 February 2020
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\bin\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\bnon\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\bet\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\best\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\bquod\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\bde\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\bunde\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\bad\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\bqui\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\bsed\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\buel\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\but\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\bcum\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\bautem\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\bsi\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\ba\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\bex\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\bsunt\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\buero\b" | wc -l
+cat Gratian1.txt Gratian2.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr '[:space:]' '\n' | egrep -v '^$' | egrep "\benim\b" | wc -l
